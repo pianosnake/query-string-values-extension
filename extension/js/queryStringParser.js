4a2149ded2db76e0fc90068aa0122e69afdd95f0
Florin Alexandrescu <pianosnake@gmail.com>
2015-02-04 22:32:19 -0700

    Decode base64 params

--- extension/js/queryStringParser.js
+++ extension/js/queryStringParser.js
@@ -1,14 +1,27 @@
 function QueryStringParser(){
 	this.regExPlusSymbol = /\+/g;
-	this.regExQuerySearch = /([^&=]+)=?([^&]*)/g;	
+	this.regExQuerySearch = /([^&=]+)=?([^&]*)/g;
+
+  function isBase64(str){
+    //regex from http://stackoverflow.com/questions/475074/regex-to-parse-or-validate-base64-data
+    var base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+    return base64Regex.test(str);
+  }
 
 	this.getValues = function(url){
 		var query = this.getQueryFromUrl(url);
 		var values = [];
 		var match;
 		while (!!(match = this.regExQuerySearch.exec(query))){
+      var key = this.decode(match[1]);
+      var val = this.decode(match[2]);
+
+      if(key === 'locations' && isBase64(val)){
+        val = atob(val);
+      }
+
       var kv = {};
-			kv[this.decode(match[1])] = this.decode(match[2]);
+      kv[key] = val;
       values.push(kv);
 		}
 		return values;
